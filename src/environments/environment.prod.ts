12c252ae09a561f252edf2a371f950b481a8ace7
hamzakargin <hamzakargin@outlook.com>
2025-03-29 22:00:29 +0300

    deploy5

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  apiUrl: process.env['NG_APP_BASE_URL'],
+  limit: 20
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  apiUrl: process.env['NG_APP_BASE_URL'],
+  limit: 20
+};
+
+//appconfig ts types eklendi
